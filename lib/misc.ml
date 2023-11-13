641ff063122ffe30ee8a825b6b4e969de4fa9f4e
Jun Furuse <jun.furuse@dailambda.jp>
2023-11-13 17:07:12 +0900

    Adds Seq.mapi

--- lib/misc.ml
+++ lib/misc.ml
@@ -80,3 +80,19 @@ end
 module Gen = struct
   type 'a t = Random.State.t -> 'a
 end
+
+module Seq = struct
+  include Seq
+
+  let mapi f t =
+    let rec aux n t =
+      fun () ->
+        match t () with
+        | Nil -> Nil
+        | Cons (x, t) ->
+            let x = f n x in
+            let t = aux (n+1) t in
+            Cons (x, t)
+    in
+    aux 0 t
+end
