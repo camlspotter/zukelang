bb44baf0fb0ba1ee2b1b3d7ce33f94bbac5b6596
Jun Furuse <jun.furuse@dailambda.jp>
2023-12-04 10:42:56 +0900

    Zk: adds field.mli

--- src/lib/zk/field.mli
+++ src/lib/zk/field.mli
@@ -1,33 +1,19 @@
 open Misclib
 
 module type S = sig
-  (** element of field *)
   type t
 
-  val pp : t printer
-
-  (** 0 *)
+  val pp : t Misclib.printer
   val zero : t
-
   val is_zero : t -> bool
-
-  (** 1 *)
   val one : t
-
   val of_int : int -> t
-
   val of_z : Z.t -> t
-
   val ( = ) : t -> t -> bool
-
   val ( + ) : t -> t -> t
-
   val ( - ) : t -> t -> t
-
   val ( * ) : t -> t -> t
-
   val ( / ) : t -> t -> t
-
   val ( ~- ) : t -> t
 
   include JSON.Conv.S with type t := t
